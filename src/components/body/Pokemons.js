2d47e77845ea30ecd08ae14264557b9a12b7542e
Rafael Pereira <rafaengcivilrj@outlook.com>
2022-11-03 18:40:13 -0300

    types adicionados

--- src/components/body/Pokemons.js
+++ src/components/body/Pokemons.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import StylePokemon from '../body/Pokemons.module.css';
 import Search from './Search';
+import TypeSelector from './TypeSelector';
 
-export default function Pokemons() {
+export default function Pokemons({ type, pokemon }) {
   return (
     <>
       <section className={StylePokemon.top}>
         <Search />
+        <div className={StylePokemon.grid}>
+          <TypeSelector {...type} {...pokemon} />
+        </div>
       </section>
     </>
   );
---
+++ src/components/body/TypesImage.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import All from '../../img/poke-type/icon-all.svg';
+import Normal from '../../img/poke-type/normal.svg';
+import Fighting from '../../img/poke-type/fighting.svg';
+import Flying from '../../img/poke-type/flying.svg';
+import Poison from '../../img/poke-type/poison.svg';
+import Ground from '../../img/poke-type/ground.svg';
+import Rock from '../../img/poke-type/rock.svg';
+import Bug from '../../img/poke-type/bug.svg';
+import Ghost from '../../img/poke-type/ghost.svg';
+import Steel from '../../img/poke-type/steel.svg';
+import Fire from '../../img/poke-type/fire.svg';
+import Water from '../../img/poke-type/water.svg';
+import Grass from '../../img/poke-type/grass.svg';
+import Electric from '../../img/poke-type/electric.svg';
+import Psychic from '../../img/poke-type/psychic.svg';
+import Ice from '../../img/poke-type/ice.svg';
+import Dragon from '../../img/poke-type/dragon.svg';
+import Dark from '../../img/poke-type/dark.svg';
+import Fairy from '../../img/poke-type/fairy.svg';
+
+const Types = {
+  pokemon: {
+    type: [
+      'All',
+      'Normal',
+      'Fighting',
+      'Flying',
+      'Poison',
+      'Ground',
+      'Rock',
+      'Bug',
+      'Ghost',
+      'Steel',
+      'Fire',
+      'Water',
+      'Grass',
+      'Electric',
+      'Psychic',
+      'Ice',
+      'Dragon',
+      'Dark',
+      'Fairy',
+    ],
+    ico: [
+      All,
+      Normal,
+      Fighting,
+      Flying,
+      Poison,
+      Ground,
+      Rock,
+      Bug,
+      Ghost,
+      Steel,
+      Fire,
+      Water,
+      Grass,
+      Electric,
+      Psychic,
+      Ice,
+      Dragon,
+      Dark,
+      Fairy,
+    ],
+  },
+};
+
+export default Types;
