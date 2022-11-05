7a309684872684233f8c6063ca0599289326f935
Rafael Pereira <rafaengcivilrj@outlook.com>
2022-11-05 10:23:18 -0300

    footer adicionado

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import StyleFooter from './Footer.module.css';
+import ImageLogo from '../../img/logo.svg';
+import ImagePokeball from '../../img/img-pokeball.png';
 
 export default function Footer() {
-  return <div></div>;
+  return (
+    <>
+      <footer>
+        <div className={StyleFooter.wrapper}>
+          <div className={StyleFooter.logo}>
+            <img src={ImageLogo} alt="logo" />
+            <img
+              src={ImagePokeball}
+              alt="logo"
+              className={StyleFooter.pokeball}
+            />
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
